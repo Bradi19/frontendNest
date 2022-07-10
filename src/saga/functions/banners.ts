0ee09ec495271b5ef9db8464bb06515598057b2e
Mykola Bok <bmo.ossystem@gmail.com>
2022-07-10 18:38:39 +0300

    Update

--- src/saga/functions/banners.ts
+++ src/saga/functions/banners.ts
@@ -1,11 +1,11 @@
 /* eslint-disable require-yield */
 import { call, put } from 'redux-saga/effects'
 import types from '../../types'
-import Axios from '../axiosRequest'
+import { Axios } from '../axiosRequest'
 
 export function* getBanners(action: any): any {
   try {
-    const result = yield call(axios.post())
-    yield put(types.banners.GET_BANNERS_SUCCESS, result)
+    const result = yield call(Axios.post())
+    yield put<any>(types.banners.GET_BANNERS_SUCCESS, result)
   } catch (e) {}
 }
